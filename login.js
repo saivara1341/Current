7620628805d448113f8a01ea8c9cff5c3065fc5c
sai_vara_1729 <144797272+saivara1341@users.noreply.github.com>
2024-06-26 20:34:20 +0530

    Update login.js

--- login.js
+++ login.js
@@ -6,7 +6,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     
     // Sample check for credentials (username: user, password: pass)
     if (username === 'ASHOK' && password === '26-06-2024') {
-        window.location.href = '4.html';
+        window.location.href = 'index.html';
     } else {
         document.getElementById('error-message').textContent = 'Invalid username or password';
     }
